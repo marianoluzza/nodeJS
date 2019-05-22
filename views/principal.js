545e13ae7a1b59104ecc0b7401df68af550de4f3
Mariano Luzza <marianoluzza@gmail.com>
2019-05-22 17:25:02 -0300

    Leer html

--- views/principal.js
+++ views/principal.js
@@ -9,3 +9,16 @@ exports.build = function(title, pagetitle, content) {
     .replace(/{pagetitle}/g, pagetitle)
     .replace(/{content}/g, content);
   };
+
+  exports.html = function(title, pagetitle, content) {  
+      
+    return ['<!doctype html>',
+    '<html lang="en">\n\n<meta charset="utf-8">\n<title>{title}</title>',
+    '<link rel="stylesheet" href="/css/estilo.css" />\n',
+    '<h1>{pagetitle}</h1>',
+    '<div id="content">{content}</div>\n\n']
+    .join('\n')
+    .replace(/{title}/g, title)
+    .replace(/{pagetitle}/g, pagetitle)
+    .replace(/{content}/g, content);
+  };

5216f2669f6fb2bf259d421092a4ae7bd8cabe84
Mariano Luzza <marianoluzza@gmail.com>
2019-06-06 17:02:09 -0300

    refactor

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require("./routes");
 
-const productController = require('./controllers/product.js');
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use("/api", api);

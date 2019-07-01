9299fa14bce3d760bd3ecdda2d4b7fd81228e8d0
Mariano Luzza <marianoluzza@gmail.com>
2019-07-01 01:17:06 -0300

    Agregado CORS para reques CROSS origing

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const api = require("./routes");
+var cors = require('cors');
+
+app.use(cors({origin: 'http://localhost:4200'}));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
